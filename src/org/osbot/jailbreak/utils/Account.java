42f9f2e3898efa0f7afe451b7560f0d1546f3d2b
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-01-24 16:42:01 -0300

    Add serial version id to ensure cross update serialization.

--- src/org/osbot/jailbreak/utils/Account.java
+++ src/org/osbot/jailbreak/utils/Account.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class Account implements Serializable {
 
+	private static final long serialVersionUID = 42L;
 	private String email, password;
 
 	public Account(final String email, final String password) {
